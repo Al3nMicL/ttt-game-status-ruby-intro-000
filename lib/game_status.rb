a6c980791191b4ab2fffa860fd11bf3b26322bc5
Alen N <nela.asus+dvcs@gmail.com>
2018-09-03 01:46:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,39 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+=begin
+ # Board visual
+ 0 | 1 | 2
+ 3 | 4 | 5
+ 6 | 7 | 8
+=end
+WIN_COMBINATIONS = [
+  [0,1,2], # Top horizontal
+  [3,4,5], # Middle horizontal
+  [6,7,8], # Bottom horizontal
+  [0,3,6], # Left vertical 
+  [1,4,7], # Middle vertical
+  [2,5,8], # Right vertical
+  [0,4,8], # \ diagonal
+  [2,4,6] # / diagonal
+]
+
+# see >> https://stackoverflow.com/a/13660352
+@positions = Proc.new { |board, letter| board.each_index.select{|i| board[i] == letter} }
+
+def won?(board)
+  pos_X = @positions[board, "X"]
+  pos_O = @positions[board, "O"]
+
+  WIN_COMBINATIONS.each do |combo|
+    if pos_X == combo
+      p pos_X
+    elsif pos_O == combo
+      p pos_O
+    else
+      nil
+    end
+  end
+end
+
+won?(board)
