90b069cba50dfe778ab5a914dacde2a3588f71f1
Alen N <nela.asus+dvcs@gmail.com>
2018-09-03 01:48:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top horizontal
   [3,4,5], # Middle horizontal
   [6,7,8], # Bottom horizontal
-  [0,3,6], # Left vertical 
+  [0,3,6], # Left vertical
   [1,4,7], # Middle vertical
   [2,5,8], # Right vertical
   [0,4,8], # \ diagonal
@@ -39,4 +39,4 @@ def won?(board)
   end
 end
 
-won?(board)
+#won?(board)
